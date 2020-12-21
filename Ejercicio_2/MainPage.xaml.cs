ace685c063a9c9bb6ef9e09c3917a4b29bc9ea44
Dagasfi <daniel.gaspar98@gmail.com>
2020-12-21 23:31:26 +0100

    [END]: Ejercicio 2

--- Ejercicio_2/MainPage.xaml.cs
+++ Ejercicio_2/MainPage.xaml.cs
@@ -26,5 +26,67 @@ namespace Ejercicio_2
         {
             this.InitializeComponent();
         }
+
+
+        private void image1_pointer_entered(object sender, PointerRoutedEventArgs e)
+        {
+            double x = e.GetCurrentPoint(image).Position.X;
+            double y = e.GetCurrentPoint(image).Position.Y;
+            this.listView.Items.Insert(0, "[ENTER] x: " + x + ", y: " + y);
+        }
+
+        private void image1_pointer_exited(object sender, PointerRoutedEventArgs e)
+        {
+            double x = e.GetCurrentPoint(image).Position.X;
+            double y = e.GetCurrentPoint(image).Position.Y;
+            this.listView.Items.Insert(0, "[OUT] x: " + x + ", y: " + y);
+        }
+
+        private void image1_pointer_moved(object sender, PointerRoutedEventArgs e)
+        {
+            double x = e.GetCurrentPoint(image).Position.X;
+            double y = e.GetCurrentPoint(image).Position.Y;
+            this.listView.Items.Insert(0, "[MOVE] x: " + x + ", y: " + y);
+        }
+
+        private void image1_pointer_pressed(object sender, PointerRoutedEventArgs e)
+        {
+            double x = e.GetCurrentPoint(image).Position.X;
+            double y = e.GetCurrentPoint(image).Position.Y;
+            this.listView.Items.Insert(0, "[PRESS] x: " + x + ", y: " + y);
+        }
+
+        private void image1_pointer_released(object sender, PointerRoutedEventArgs e)
+        {
+            double x = e.GetCurrentPoint(image).Position.X;
+            double y = e.GetCurrentPoint(image).Position.Y;
+            this.listView.Items.Insert(0, "[RELEASE] x: " + x + ", y: " + y);
+        }
+
+        private void image1_pointer_wheel(object sender, PointerRoutedEventArgs e)
+        {
+            double x = e.GetCurrentPoint(image).Position.X;
+            double y = e.GetCurrentPoint(image).Position.Y;
+            this.listView.Items.Insert(0, "[WHEEL] x: " + x + ", y: " + y);
+        }
+
+        private void image1_doubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            // hacer la imagen totalmente visible.
+            image.Opacity = 1;
+        }
+
+        private void image1_tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //Bajar la transparencia un 10%.
+            image.Opacity = image.Opacity - 0.1;
+        }
+
+        private void image1_holding(object sender, HoldingRoutedEventArgs e)
+        {
+            //Borrar el listview.
+            this.listView.Items.Clear();
+        }
     }
 }
+
